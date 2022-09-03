20ec6d8eff44e0d8609ce1773b9f621b93973c8d
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-09-02 19:23:40 -0500

    show the contact for while site is WIP & analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { CssBaseline } from '@mui/material'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CssBaseline />
+      <div className='App'>
+        <iframe
+          title='Contact form'
+          src='https://docs.google.com/forms/d/e/1FAIpQLSci6KwpXOD4a5s62ZaRO6Jao_7gjSGa1OsT-_wjxhE7QoPB8w/viewform?embedded=true'
+          width={640}
+          height={1220}
+          frameBorder={0}
+          marginHeight={0}
+          marginWidth={0}>
+          Loading…
+        </iframe>
+      </div>
+    </>
+  )
 }
-
-export default App;
--- src/util/analytics.ts
+++ src/util/analytics.ts
@@ -1,19 +1,30 @@
-import ReactGA from "react-ga4"
 import type { UaEventOptions } from 'react-ga4/types/ga4'
 
-const { REACT_APP_GA_MEASURMENT_ID } = process.env
+import ReactGA from "react-ga4"
+import reportWebVitals from './reportWebVitals'
 
-if (REACT_APP_GA_MEASURMENT_ID)
-  ReactGA.initialize(REACT_APP_GA_MEASURMENT_ID)
+const { REACT_APP_GA_MEASURMENT_ID: GA_MEASURMENT_ID } = process.env
+
+if (GA_MEASURMENT_ID) {
+  ReactGA.initialize(GA_MEASURMENT_ID)
+  reportWebVitals(({ id, name, value }) => event({
+    category: 'vitals',
+    action: name,
+    value: Math.round(value * (name === 'CLS' ? 1000 : 0)), // must be int
+    label: id, // unique to page load
+    nonInteraction: true, // no bounce rate
+  }))
+}
+
 
 /** Send a page view */
 export default function () {
-  if (REACT_APP_GA_MEASURMENT_ID)
+  if (GA_MEASURMENT_ID)
     ReactGA.send("pageview")
 }
 
 /** Send a custom event */
 export function event(args: UaEventOptions) {
-  if (REACT_APP_GA_MEASURMENT_ID)
+  if (GA_MEASURMENT_ID)
     ReactGA.event(args)
 }
