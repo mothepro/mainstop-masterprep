ad25a3dd5174221fea6c06d8248df8f30b1b98b8
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-09-04 12:25:15 -0500

    include dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, Dialog, DialogContent } from '@mui/material'
+import { event } from './util/analytics'
+import Testimonials from './components/Testimonials'
 
 export default function App() {
   return (
     <>
       <CssBaseline />
       <div className='App'>
-        <iframe
-          title='Contact form'
-          src='https://docs.google.com/forms/d/e/1FAIpQLSci6KwpXOD4a5s62ZaRO6Jao_7gjSGa1OsT-_wjxhE7QoPB8w/viewform?embedded=true'
-          width={640}
-          height={1220}
-          frameBorder={0}
-          marginHeight={0}
-          marginWidth={0}>
-          Loading…
-        </iframe>
+        <Dialog
+          fullWidth
+          maxWidth='md'
+          open={false}
+          onClose={() => event({ category: 'dialog', action: 'close' })}
+          sx={{
+            textAlign: 'center',
+          }}
+        >
+          <DialogContent sx={{ minWidth: 640 }}>
+            <iframe
+              title='Contact form'
+              src='https://docs.google.com/forms/d/e/1FAIpQLSci6KwpXOD4a5s62ZaRO6Jao_7gjSGa1OsT-_wjxhE7QoPB8w/viewform?embedded=true'
+              width={640}
+              height={1220}
+              frameBorder={0}
+              marginHeight={0}
+              marginWidth={0}>
+              Loading…
+            </iframe>
+          </DialogContent>
+        </Dialog>
+        <Testimonials/>
       </div>
     </>
   )
